633fd8e51b22ef9459357610cedaaa320788cc6b
Alexandro T. Netto <alex.t.netto@gmail.com>
2023-10-08 02:32:36 +0200

    feat: add mainnet contract addresses

---
+++ constants/contractAddresses.js
@@ -0,0 +1,12 @@
+export const contractAddresses = {
+  mainnet: {
+    OldETHRegistrarController: "0x283Af0B28c62C092C9727F1Ee09c02CA627EB7F5",
+    NewETHRegistrarController: "0x253553366Da8546fC250F225fe3d25d0C782303b",
+    BaseRegistrar: "0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85",
+    DNSRegistrar: "0x58774Bb8acD458A640aF0B88238369A167546ef2",
+    NameWrapper: "0xD4416b13d2b3a9aBae7AcD5D6C2BbDBE25686401",
+    PublicResolver: "0x231b0Ee14048e9dCcD1d247744d114a4EB5E8E63",
+    Registry: "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e",
+    ReverseRegistrar: "0xa58E81fe9b61B5c3fE2AFD33CF304c454AbFc7Cb",
+  },
+};
